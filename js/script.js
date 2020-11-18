81a62680d7ff2b65a127f5af8a515acefe91b39e
Natália <verasnatali4@gmail.com>
2020-11-18 17:36:38 +0100

    Delete item working

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 var app = new Vue({
   el: "#root",
   data: {
+    index: 0,
     inputTask: [],
-    allTasks: [],
+    allTasks: []
   },
   methods: {
     createTask: function () {
 
       this.allTasks.push(this.inputTask);
-      console.log(this.allTasks);
+      // console.log(this.allTasks);
 
     },
-    trash: function() {
-      console.log('delete task');
+    trash: function(index) {
+
+      this.allTasks.splice(index, 1);
+
+      // console.log(this.allTasks);
+
     }
 
   }
