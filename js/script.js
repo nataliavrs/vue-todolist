6fa7e6c5194ea5b91beb0c0e9bacf609a372afe0
Natália <verasnatali4@gmail.com>
2020-11-18 17:51:07 +0100

    Completed basic assignment

--- js/script.js
+++ js/script.js
@@ -2,26 +2,21 @@ var app = new Vue({
   el: "#root",
   data: {
     index: 0,
-    inputTask: [],
+    inputTask: "",
     allTasks: []
   },
   methods: {
     createTask: function () {
 
       this.allTasks.push(this.inputTask);
-      // console.log(this.allTasks);
 
     },
     trash: function(index) {
 
       this.allTasks.splice(index, 1);
 
-      // console.log(this.allTasks);
-
     }
 
   }
-
-
 
 });
