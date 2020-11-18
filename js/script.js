c896d27ef538049296bcb379b5cd51fbd4e7c2da
Natália <verasnatali4@gmail.com>
2020-11-18 17:26:48 +0100

    Working new item to list

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 var app = new Vue({
   el: "#root",
   data: {
-    inputTask: "",
+    inputTask: [],
     allTasks: [],
   },
   methods: {
-    trash: function() {
-      console.log('delete task');
-    },
     createTask: function () {
 
-      this.allTasks.push('inputTask')
+      this.allTasks.push(this.inputTask);
       console.log(this.allTasks);
 
+    },
+    trash: function() {
+      console.log('delete task');
     }
-
 
   }
 
