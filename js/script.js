6b04cf4c701207e737a5f7aaaf39a7b29345c177
Natália <verasnatali4@gmail.com>
2020-11-19 11:58:21 +0100

    Trying to change style when checked

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 var app = new Vue({
   el: "#root",
   data: {
+    doneTask: "",
     inputTask: "",
     allTasks: [],
   },
   methods: {
     createTask: function () {
 
-      this.allTasks.push(this.inputTask);
+      if (this.inputTask == "") {
+        // nothing happens if user doesn't write a task
+      } else {
+        this.allTasks.push(this.inputTask);
+      }
 
     },
     deleteTask: function(index) {
 
       this.allTasks.splice(index, 1);
+
+    },
+    checkItem: function (index) {
+
+      this.doneTask = 'checked';
+
+      console.log(index);
 
     }
 
